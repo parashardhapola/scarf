07321dc7e1569765439a9c96c31d62c4b4936e2a
parashardhapola <parashar.dhapola@gmail.com>
2020-06-05 14:49:52 +0200

    removed dead imports

--- scarf/umap.py
+++ scarf/umap.py
@@ -7,9 +7,7 @@ import numpy as np
 from sklearn.utils import check_random_state
 from umap.umap_ import make_epochs_per_sample, find_ab_params
 from umap.layouts import optimize_layout_euclidean
-import numba
 import locale
-from tqdm import tqdm
 
 locale.setlocale(locale.LC_NUMERIC, "C")
 
