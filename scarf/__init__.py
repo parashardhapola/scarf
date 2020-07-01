7d9443a753ef4b1f36d69ebfbb89cb49c0993495
parashardhapola <parashar.dhapola@gmail.com>
2020-07-01 23:59:11 +0200

    disbled dask distributed warnings about gc

--- scarf/__init__.py
+++ scarf/__init__.py
@@ -1,6 +1,10 @@
 import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
 from threadpoolctl import threadpool_limits
+import logging
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+logger = logging.getLogger("distributed.utils_perf")
+logger.setLevel(logging.ERROR)
 
 from .datastore import *
 from .readers import *
