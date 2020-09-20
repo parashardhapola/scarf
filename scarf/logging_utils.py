8928e8e334f67ca8539bc069dcc39ad20d7349a3
parashardhapola <parashar.dhapola@gmail.com>
2020-09-20 11:08:43 +0200

    set log level to info

--- scarf/logging_utils.py
+++ scarf/logging_utils.py
@@ -2,6 +2,6 @@ from loguru import logger
 import sys
 
 logger.remove()
-logger.add(sys.stdout, colorize=True, format="<level>{level}</level>: {message}")
+logger.add(sys.stdout, colorize=True, format="<level>{level}</level>: {message}", level="INFO")
 
 __all__ = [logger]
