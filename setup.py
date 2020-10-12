141a6c5fd1ce6cce495e7f16d70043e09e8f3f01
parashardhapola <parashar.dhapola@gmail.com>
2020-10-13 01:39:49 +0200

    updated dependencies. removed dask distributed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 if __name__ == "__main__":
-    CLASSIFIERS = [
+    classifiers = [
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
@@ -15,8 +15,10 @@ if __name__ == "__main__":
         "Programming Language :: Python :: 3",
         'Programming Language :: Python :: 3.7',
     ]
-    KEYWORDS = ['store']
-    VERSION = open('VERSION').readline().rstrip('\n')
+    keywords = ['store']
+    version = open('VERSION').readline().rstrip('\n')
+    install_requires = ['pybind11'] + [x.strip() for x in open('requirements.txt')] + ['dask[array]']
+    dependency_links = ['https://github.com/fraenkel-lab/pcst_fast/tarball/master#egg=pcst_fast-1.0.7']
     setup(
         name='scarf',
         description='scarf',
@@ -25,10 +27,11 @@ if __name__ == "__main__":
         author_email='parashar.dhapola@gmail.com',
         url='https://github.com/parashardhapola/scarf',
         license='BSD 3-Clause',
-        classifiers=CLASSIFIERS,
-        keywords=KEYWORDS,
-        install_requires=[x.strip() for x in open('requirements.txt')],
-        version=VERSION,
+        classifiers=classifiers,
+        keywords=keywords,
+        install_requires=install_requires,
+        dependency_links=dependency_links,
+        version=version,
         packages=find_packages(exclude=['data', 'bin']),
         include_package_data=False
     )
