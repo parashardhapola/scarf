795ddd6631e39caa172babe675f577ebe8091a64
Stefan Lang <Stefan.Lang@med.lu.se>
2020-10-13 10:09:17 +0200

    test file from https://github.com/maximilianh/cellBrowser/blob/master/sampleData/pbmc_small/anndata.h5ad

--- tests/test_readh5.py
+++ tests/test_readh5.py
@@ -4,4 +4,8 @@ import pytest
 
 
 def test_H5adReader( ):
-    assert scarf.H5adReader( 'testData/tiny2.h5ad')
+    reader = scarf.H5adReader( 'testData/anndata.h5ad')
+    writer = scarf.H5adToZarr(reader, zarr_fn='testData/TestZarr', chunk_size=(1000, 1000))
+    writer.dump(batch_size=1000)
+    dr = scarf.DataStore( f'testData/TestZarr' )
+    dr
