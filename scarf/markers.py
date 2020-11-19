parashardhapola/scarf

commit e569d92daf7b6b75ff2ad8b657909b080f9140d6

Fixes #11

scarf/markers.py
@@ -13,10 +13,9 @@ def find_markers_by_rank(assay: Assay, group_key: str, subset_key: str, nthreads
     @jit()
     def calc_mean_rank(v):
         #  TODO: fix for non-numeric index set
-        n_indices = index_set.shape[0]
         r = np.ones(n_indices)
         for x in range(n_indices):
-            r[x] = v[indices == index_set[x]].mean()
+            r[x] = v[int_index_set == x].mean()
         return r / r.sum()
 
     def mean_rank_wrapper(v):
@@ -25,13 +24,17 @@ def find_markers_by_rank(assay: Assay, group_key: str, subset_key: str, nthreads
     c_idx = assay.cells.active_index(subset_key)
     indices = assay.cells.fetch(group_key, subset_key)
     index_set = np.array(sorted(set(indices)))
+    n_indices = len(index_set)
+    idx_map = dict(zip(index_set, range(n_indices)))
+    int_index_set = np.array([idx_map[x] for x in index_set])
+
     data = assay.normed(cell_idx=c_idx).T
     gene_names = np.split(assay.feats.fetch('ids'), np.cumsum(data.chunks[0]))[:-1]
     results = {x: [] for x in index_set}
     for i, names in tqdm(zip(data.blocks, gene_names), desc='Finding markers', total=data.numblocks[0]):
         val = pd.DataFrame(controlled_compute(i, nthreads), index=names).T.rank(method='dense').astype(int)
         res = val.apply(mean_rank_wrapper)
-        res = res.T[(res < threshold).sum() != len(index_set)].T
+        res = res.T[(res < threshold).sum() != n_indices].T
         res.index = index_set
         # The following line was commented out to return results as gene IDs rather than gene names
         # res.columns = assay.feats.table.names[assay.feats.table.ids.isin(res.columns)].values

commit 8f04a61905c6c33e129f131db770b31ec6ad5c7c

fixes critical bugs introduced in e569d92

scarf/markers.py
@@ -1,46 +1,52 @@
 from .assay import Assay
 from .utils import controlled_compute
+from .logging_utils import logger
+from numba import jit
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
 
 __all__ = ['find_markers_by_rank']
 
 
-def find_markers_by_rank(assay: Assay, group_key: str, subset_key: str, nthreads: int, threshold: float = 0.25) -> dict:
-    from numba import jit
-    import numpy as np
-    import pandas as pd
-    from tqdm import tqdm
+def find_markers_by_rank(assay: Assay, group_key: str, cell_key: str,
+                         nthreads: int, threshold: float = 0.25) -> dict:
 
     @jit()
     def calc_mean_rank(v):
-        #  TODO: fix for non-numeric index set
-        r = np.ones(n_indices)
-        for x in range(n_indices):
-            r[x] = v[int_index_set == x].mean()
+        r = np.ones(n_groups)
+        for x in range(n_groups):
+            r[x] = v[int_indices == x].mean()
         return r / r.sum()
 
     def mean_rank_wrapper(v):
         return calc_mean_rank(v.values)
 
-    c_idx = assay.cells.active_index(subset_key)
-    indices = assay.cells.fetch(group_key, subset_key)
-    index_set = np.array(sorted(set(indices)))
-    n_indices = len(index_set)
-    idx_map = dict(zip(index_set, range(n_indices)))
-    int_index_set = np.array([idx_map[x] for x in index_set])
+    groups = assay.cells.fetch(group_key, cell_key)
+    group_set = sorted(set(groups))
+    n_groups = len(group_set)
+    # Since, numba needs int arrays to work properly but the dtype of 'groups' may not be integer type
+    # Hence we need to create a indexed version of 'groups'
+    idx_map = dict(zip(group_set, range(n_groups)))
+    rev_idx_map = {v:k for k,v in idx_map.items()}
+    int_indices = np.array([idx_map[x] for x in groups])
 
-    data = assay.normed(cell_idx=c_idx).T
+    # We create dask array of the normalized data. The array is transposed in order to allow chunk-wise iteration
+    # over genes rather than cells.
+    data = assay.normed(cell_idx=assay.cells.active_index(cell_key)).T
+    # All the genes to be tested are split into arrays. Each array being of same size as the transposed chunk
+    # dimension
     gene_names = np.split(assay.feats.fetch('ids'), np.cumsum(data.chunks[0]))[:-1]
-    results = {x: [] for x in index_set}
+    results = {x: [] for x in group_set}
+    
     for i, names in tqdm(zip(data.blocks, gene_names), desc='Finding markers', total=data.numblocks[0]):
         val = pd.DataFrame(controlled_compute(i, nthreads), index=names).T.rank(method='dense').astype(int)
         res = val.apply(mean_rank_wrapper)
-        res = res.T[(res < threshold).sum() != n_indices].T
-        res.index = index_set
-        # The following line was commented out to return results as gene IDs rather than gene names
-        # res.columns = assay.feats.table.names[assay.feats.table.ids.isin(res.columns)].values
-        res = res.T
+        # Removing genes that were below the threshold in all the groups
+        res = res.T[(res < threshold).sum() != n_groups]
         for j in res:
-            results[j].append(res[j][res[j] > threshold])
+            results[rev_idx_map[j]].append(res[j][res[j] > threshold])
     for i in results:
         results[i] = pd.concat(results[i]).sort_values(ascending=False)
     return results