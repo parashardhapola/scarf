6c94e058a60315033aba799271a1c8697a4926e3
parashardhapola <parashar.dhapola@gmail.com>
2020-11-25 23:57:27 +0100

    run_topacedo_sampler added

--- scarf/datastore.py
+++ scarf/datastore.py
@@ -1724,11 +1724,12 @@ class DataStore:
         return None
 
     def run_topacedo_sampler(self, *, from_assay: str = None, cell_key: str = 'I', feat_key: str = None,
-                     cluster_key: str = None, density_key: str = None,
-                     min_edge_weight: float = -1, seed_frac: float = 0.05,
-                     dynamic_seed_frac: bool = True, dynamic_frac_multiplier: float = 2,
-                     min_nodes: int = 3, rewards: tuple = (3, 0.1),
-                     rand_state: int = 4466, return_vals: bool = False, label: str = 'sketched'):
+                             cluster_key: str = None, density_depth: int = 2,
+                             sampling_rate: float = 0.1, min_cells_per_group: int = 3,
+                             min_sr: float = 0.01, seed_reward: float = 3.0, non_seed_reward: float = 0,
+                             save_sampling_key: str = 'sketched', save_density_key: str = 'cell_density',
+                             save_seeds_key: str = 'sketch_seeds', rand_state: int = 4466,
+                             return_edges: bool = False) -> Union[None, List]:
         """
         Perform sub-sampling (aka sketching) of cells using TopACeDo algorithm. Sub-sampling required
         that cells are partitioned in cluster already. Since, sub-sampling is dependent on cluster information, having,
@@ -1740,29 +1741,31 @@ class DataStore:
             feat_key: Feature key. Should be same as the one that was used in the desired graph. By default the latest
                        used feature for the given assay will be used.
             cluster_key: Name of the column in cell metadata table where cluster information is stored.
-            density_key: Name of the column in cell metadata table where neighbourhood density values are stored.
-                         Only required if `dynamic_seed_frac` is True.
-            min_edge_weight: This parameter is forwarded to `load_graph` and is same as there. (Default value: -1)
-            seed_frac: Fraction of cells to be sampled from each cluster. Should be greater than 0 and less than 1.
-                       (Default value: 0.05)
-            dynamic_seed_frac: if True, then dynamic sampling rate rate will be used. Dynamic sampling takes the mean
-                               node density into account while sampling cells from each cluster (default value: True)
-            dynamic_frac_multiplier: A scalar value used an multiplier to increase the dynamic sampling rate
-            min_nodes: Minimum number of nodes to be sampled from each cluster. (Default value: 3)
-            rewards: Reward values for seed and non-seed nodes. A tuple of two values is provided, first for seed nodes
-                     and second for non-seed nodes. (Default value: (3, 0.1))
-            rand_state: A random values to set seed while sampling cells from a cluster randomly.
-            return_vals: If True, then steiner nodes and edges are returned. (Default value: False)
-            label: base label for saving values into a cell metadata column (Default value: 'sketched')
-
+            density_depth: Same as 'search_depth' parameter in `calc_neighbourhood_density`. (Default value: 2)
+            sampling_rate: Maximum fraction of cells to sample from each group. The effective sampling rate is lower
+                           than this value depending on the neighbourhood density of the cells.
+                           Should be greater than 0 and less than 1. (Default value: 0.1)
+            min_cells_per_group: Minimum number of cells to sample from each group. (Default value: 3)
+            min_sr: Minimum sampling rate. Effective sampling rate is not allowed to be lower than this value.
+                    (Default value: 0.01)
+            seed_reward: Reward/prize value for seed nodes. (Default value: 3)
+            non_seed_reward: Reward/prize for non-seed nodes. (Default value: 0.1)
+            save_sampling_key: base label for marking the cells that were sampled into a cell metadata column
+                               (Default value: 'sketched')
+            save_density_key: base label for saving the cell neighbourhood densities into a cell metadata column
+                              (Default value: 'cell_density')
+            save_seeds_key: base label for saving the seed cells (identified by topacedo sampler) into a cell
+                            metadata column (Default value: 'sketch_seeds')
+            rand_state: A random values to set seed while sampling cells from a cluster randomly. (Default value: 4466)
+            return_edges: If True, then steiner nodes and edges are returned. (Default value: False)
         Returns:
 
         """
         try:
-            from topacedo import cell_sampler
+            from topacedo import TopacedoSampler
         except ImportError:
             logger.error("Could not find topacedo package")
-            return False
+            return None
 
         if from_assay is None:
             from_assay = self._defaultAssay
@@ -1771,35 +1774,31 @@ class DataStore:
         if cluster_key is None:
             raise ValueError("ERROR: Please provide a value for cluster key")
         clusters = pd.Series(self.cells.fetch(cluster_key, cell_key))
-        graph = self.load_graph(from_assay, cell_key, feat_key, 'csr', min_edge_weight, False, False)
+        graph = self.load_graph(from_assay, cell_key, feat_key, 'csr', -1, False, False)
         if len(clusters) != graph.shape[0]:
             raise ValueError(f"ERROR: cluster information exists for {len(clusters)} cells while graph has "
                              f"{graph.shape[0]} cells.")
-        if dynamic_seed_frac and density_key is None:
-            logger.warning("`dynamic_seed_frac` will be ignored because node_density has not been calculated.")
-            dynamic_seed_frac = False
-        if dynamic_seed_frac:
-            if density_key not in self.cells.table:
-                raise ValueError(f"ERROR: {density_key} not found in cell metadata table")
-            else:
-                cff = self.cells.table[self.cells.table.I].groupby(cluster_key)[density_key].median()
-                cff = (cff - cff.min()) / (cff.max() - cff.min())
-                cff = 1 - cff
-                cff = dynamic_frac_multiplier * cff
-        else:
-            n_clusts = clusters.nunique()
-            cff = pd.Series(np.zeros(n_clusts), index=list(range(1, n_clusts + 1)))
-        steiner_nodes, steiner_edges = cell_sampler(
-            graph=graph, clusters=clusters, seed_frac=seed_frac, cluster_factor=cff, min_nodes=min_nodes,
-            rewards=rewards, pruning_method='strong', rand_state=rand_state)
+        sampler = TopacedoSampler(graph, clusters.values, density_depth, sampling_rate, min_cells_per_group,
+                                  min_sr, seed_reward, non_seed_reward, 1, rand_state)
+        nodes, edges = sampler.run()
         a = np.zeros(self.cells.table[cell_key].values.sum()).astype(bool)
-        a[steiner_nodes] = True
-
-        key = self._col_renamer(from_assay, cell_key, label)
+        a[nodes] = True
+        key = self._col_renamer(from_assay, cell_key, save_sampling_key)
         self.cells.add(key, a, fill_val=False, key=cell_key, overwrite=True)
-        logger.info(f"Sketched cells saved with keyname '{key}'")
-        if return_vals:
-            return steiner_nodes, steiner_edges
+        logger.info(f"Sketched cells saved under column '{key}'")
+
+        key = self._col_renamer(from_assay, cell_key, save_density_key)
+        self.cells.add(key, sampler.densities, key=cell_key, overwrite=True)
+        logger.info(f"Cell neighbourhood densities saved under column: '{key}'")
+
+        a = np.zeros(self.cells.table[cell_key].values.sum()).astype(bool)
+        a[sampler.seeds] = True
+        key = self._col_renamer(from_assay, cell_key, save_seeds_key)
+        self.cells.add(key, a, fill_val=False, key=cell_key, overwrite=True)
+        logger.info(f"Seed cells saved under column: '{key}'")
+
+        if return_edges:
+            return edges
 
     def make_bulk(self, from_assay: str = None, group_key: str = None, pseudo_reps: int = 3, null_vals: list = None,
                   random_seed: int = 4466) -> pd.DataFrame:
