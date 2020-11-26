e79626aa66e37e3502fb0c3531d4d5386996d4ef
parashardhapola <parashar.dhapola@gmail.com>
2020-11-26 02:53:48 +0100

    fixed for memory efficiency

--- scarf/markers.py
+++ scarf/markers.py
@@ -11,7 +11,7 @@ __all__ = ['find_markers_by_rank']
 
 
 def find_markers_by_rank(assay: Assay, group_key: str, cell_key: str,
-                         nthreads: int, threshold: float = 0.25) -> dict:
+                         nthreads: int, threshold: float = 0.25, gene_batch_size: int = 50) -> dict:
 
     @jit()
     def calc_mean_rank(v):
@@ -29,19 +29,17 @@ def find_markers_by_rank(assay: Assay, group_key: str, cell_key: str,
     # Since, numba needs int arrays to work properly but the dtype of 'groups' may not be integer type
     # Hence we need to create a indexed version of 'groups'
     idx_map = dict(zip(group_set, range(n_groups)))
-    rev_idx_map = {v:k for k,v in idx_map.items()}
+    rev_idx_map = {v: k for k, v in idx_map.items()}
     int_indices = np.array([idx_map[x] for x in groups])
 
-    # We create dask array of the normalized data. The array is transposed in order to allow chunk-wise iteration
-    # over genes rather than cells.
-    data = assay.normed(cell_idx=assay.cells.active_index(cell_key)).T
-    # All the genes to be tested are split into arrays. Each array being of same size as the transposed chunk
-    # dimension
-    gene_names = np.split(assay.feats.fetch('ids'), np.cumsum(data.chunks[0]))[:-1]
+    data = assay.normed(cell_idx=assay.cells.active_index(cell_key))
+    gene_ids = assay.feats.fetch('ids')
+    chunks = np.array_split(np.arange(0, data.shape[1]), int(data.shape[1]/gene_batch_size))
+
     results = {x: [] for x in group_set}
-    
-    for i, names in tqdm(zip(data.blocks, gene_names), desc='Finding markers', total=data.numblocks[0]):
-        val = pd.DataFrame(controlled_compute(i, nthreads), index=names).T.rank(method='dense').astype(int)
+    for chunk in tqdm(chunks, desc='Finding markers', total=len(chunks)):
+        val = pd.DataFrame(controlled_compute(data[:, chunk], nthreads),
+                           columns=gene_ids[chunk]).rank(method='dense').astype(int)
         res = val.apply(mean_rank_wrapper)
         # Removing genes that were below the threshold in all the groups
         res = res.T[(res < threshold).sum() != n_groups]
