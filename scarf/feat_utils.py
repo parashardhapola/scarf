042c02a57b4a1d1d275248896b2f0410dddff397
parashardhapola <parashar.dhapola@gmail.com>
2020-11-30 10:44:15 +0100

    added score_features

--- scarf/feat_utils.py
+++ scarf/feat_utils.py
@@ -41,3 +41,52 @@ def fit_lowess(a, b, n_bins: int, lowess_frac: float) -> np.ndarray:
         for idx in indices:
             fixed_var[idx] = np.e ** (stats.b[idx] - bcf)
     return np.array([fixed_var[x] for x in range(len(a))])
+
+
+def score_features(assay, feature_list: List[str], ctrl_size: int = 50,
+                   n_bins: int = 25, rand_seed: int = 0) -> np.ndarray:
+    """
+    Score a set of genes [Satija15]_.
+    The score is the average expression of a set of genes subtracted with the
+    average expression of a reference set of genes. The reference set is
+    randomly sampled from the `gene_pool` for each binned expression value.
+
+    This reproduces the approach in Seurat [Satija15]_ and has been implemented
+    for Scanpy by Davide Cittaro.
+
+    This function is adapted from Scanpy's `score_genes`.
+
+    Args:
+        assay:
+        feature_list:
+        ctrl_size:
+        n_bins:
+        rand_seed:
+
+    Returns:
+
+    """
+    def _name_to_ids(i):
+        x = assay.feats.table.reindex(assay.feats.get_idx_by_names(i))
+        x = x[x.I]
+        return x.ids.values
+
+    def _calc_mean(i):
+        idx = sorted(assay.feats.get_idx_by_ids(i))
+        return assay.normed(feat_idx=idx).mean(axis=1).compute()
+
+    feature_list = set(_name_to_ids(feature_list))
+    obs_avg = pd.Series(assay.z.summary_stats_I.avg[:], index=assay.feats.fetch('ids'))
+    n_items = int(np.round(len(obs_avg) / (n_bins - 1)))
+
+    # Made following more linter friendly
+    # obs_cut = obs_avg.rank(method='min') // n_items
+    obs_cut: pd.Series = obs_avg.rank(method='min').divide(n_items).astype(int)
+
+    control_genes = set()
+    for cut in np.unique(obs_cut.loc[feature_list]):
+        # Replaced np.random.shuffle with pandas' sample method
+        r_genes = obs_cut[obs_cut == cut].sample(n=ctrl_size, random_state=rand_seed)
+        control_genes.update(set(r_genes.index))
+    control_genes = control_genes - feature_list
+    return _calc_mean(list(feature_list)) - _calc_mean(list(control_genes))
