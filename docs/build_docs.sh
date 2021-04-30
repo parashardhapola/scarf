d457e803fb6d948f0758df7d2fcf2860da067e40
parashardhapola <parashar.dhapola@gmail.com>
2021-04-30 10:56:25 +0200

    restrucuted vignettes

--- docs/build_docs.sh
+++
@@ -1,4 +0,0 @@
-bash ../vignettes/convert_to_notebook.bash
-cp ../vignettes/*.ipynb -t ./source/
-make html
-rm ./source/*.ipynb
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,7 +70,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints', 'vignettes/dev']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
