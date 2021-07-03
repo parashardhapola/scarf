ec587c7d52d8b5e5ec6996eba6d72bd07dd206dc
Parashar <parashardhapola@users.noreply.github.com>
2021-07-03 21:08:32 +0200

    setting up testing

---
+++ scarf/tests/test_writers.py
@@ -0,0 +1,14 @@
+import pytest
+import os
+
+
+@pytest.fixture
+def pbmc_writer(pbmc_reader):
+    from ..writers import CrToZarr
+
+    fn = os.path.join('scarf', 'tests', 'datasets', '1K_pbmc_citeseq.zarr')
+    return CrToZarr(pbmc_reader, zarr_fn=fn)
+
+
+def test_write_pbmc(pbmc_writer):
+    pbmc_writer.dump()
