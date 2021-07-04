37edffc0754a6479a4435f79be26feda4f61af16
Parashar <parashardhapola@users.noreply.github.com>
2021-07-05 01:42:40 +0200

    added tests for MetaData class

--- scarf/tests/test_writers.py
+++ scarf/tests/test_writers.py
@@ -6,7 +6,7 @@ import os
 def pbmc_writer(pbmc_reader):
     from ..writers import CrToZarr
 
-    fn = os.path.join('scarf', 'tests', 'datasets', '1K_pbmc_citeseq.zarr')
+    fn = os.path.join('scarf', 'tests', 'datasets', 'dummy_1K_pbmc_citeseq.zarr')
     return CrToZarr(pbmc_reader, zarr_fn=fn)
 
 
