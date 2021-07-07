cd051797099bb61f01c1078c824e32826cdfcdb9
Parashar <parashardhapola@users.noreply.github.com>
2021-07-07 22:08:10 +0200

    addedd test_h5ad_reader

--- scarf/tests/test_readers.py
+++ scarf/tests/test_readers.py
@@ -3,3 +3,8 @@ def test_read_pbmc(pbmc_reader):
     assert pbmc_reader.nFeatures == 36611
     n_assay_feats = list(pbmc_reader.assayFeats.T.nFeatures.values)
     assert n_assay_feats == [36601, 10]
+
+
+def test_h5ad_reader(h5ad_reader):
+    assert h5ad_reader.nCells == 3696 == len(h5ad_reader.cell_ids())
+    assert h5ad_reader.nFeatures == 27998 == len(h5ad_reader.feat_names())
