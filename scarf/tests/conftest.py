701302b2c6c6eb260ce9950c8736b70b8e70cf48
Parashar <parashardhapola@users.noreply.github.com>
2021-07-07 22:07:42 +0200

    added h5ad_reader

--- scarf/tests/conftest.py
+++ scarf/tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import shutil
 
 
 @pytest.fixture
@@ -8,3 +9,18 @@ def pbmc_reader(scope="module"):
 
     fn = os.path.join('scarf', 'tests', 'datasets', '1K_pbmc_citeseq.h5')
     return CrH5Reader(fn, 'rna')
+
+
+@pytest.fixture
+def h5ad_reader(scope="module"):
+    from ..readers import H5adReader
+    from ..downloader import fetch_dataset
+
+    sample = 'bastidas-ponce_4K_pancreas-d15_rnaseq'
+    out_dir = os.path.join('scarf', 'tests', 'datasets')
+    fetch_dataset(sample, save_path=out_dir)
+    h5ad_fn = os.path.join(out_dir, sample, 'data.h5ad')
+    reader = H5adReader(h5ad_fn)
+    yield reader
+    reader.h5.close()
+    shutil.rmtree(os.path.join(out_dir, sample))
