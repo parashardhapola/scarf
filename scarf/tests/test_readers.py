de7d08664dde6c7dc65d355dddaa5a57738530f8
parashardhapola <parashar.dhapola@gmail.com>
2021-08-19 16:46:01 +0200

    removed tests for MtxReader and MtxToZarr

--- scarf/tests/test_readers.py
+++ scarf/tests/test_readers.py
@@ -10,11 +10,6 @@ def test_crdir_reader(crdir_reader):
     assert crdir_reader.nFeatures == 36601  # Does not contain 10 ADTs
 
 
-def test_mtx_reader(mtx_reader):
-    assert mtx_reader.nCells == 892
-    assert mtx_reader.nFeatures == 36601  # Does not contain 10 ADTs
-
-
 def test_h5ad_reader(h5ad_reader):
     assert h5ad_reader.nCells == 3696 == len(h5ad_reader.cell_ids())
     assert h5ad_reader.nFeatures == 27998 == len(h5ad_reader.feat_names())
